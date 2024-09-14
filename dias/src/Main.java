a66bcbcd79be83a9a8d007f39329841e331304ef
LOURIVAL RODRIGUES DE SOUSA NETO <vexlourival1234@gmail.com>
2024-09-14 12:30:10 -0300

    Add files via upload

---
+++ dias/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        String day = "quinta";
+
+        switch (day){
+            case "segunda": System.out.println("hoje é segunda");
+            break;
+            case "terça": System.out.println("hoje é terça");
+            break;
+            case "quarta": System.out.println("hoje é quarta");
+            break;
+            case "quinta":System.out.println("hoje é quinta");
+            break;
+            case "sexta":System.out.println("hoje é sexta");
+            break;
+            default:System.out.println("isso não é um dia");
+        }
+
+    }
+}
