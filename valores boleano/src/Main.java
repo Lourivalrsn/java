a6d0ed945cac399386ecf7b6be04233324b1a0d9
LOURIVAL RODRIGUES DE SOUSA NETO <vexlourival1234@gmail.com>
2024-09-14 12:28:59 -0300

    Add files via upload

---
+++ valores boleano/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+public class Main {
+    public static void main(String[] args) {
+        Random Random = new Random();
+
+
+        //int x = Random.nextInt(6);
+        //double y = Random.nextDouble(78.21);
+        boolean z = Random.nextBoolean();
+
+        //System.out.println(y);
+        //System.out.println(x);
+        System.out.println(z);
+    }
+
+}
