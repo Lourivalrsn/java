494302430ed8d75ed91049d9da35acd92f8a0a7f
LOURIVAL RODRIGUES DE SOUSA NETO <vexlourival1234@gmail.com>
2024-09-14 12:30:46 -0300

    Add files via upload

---
+++ tempo/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int temp = 20;
+
+        if(temp>30){
+            System.out.println("esta quente la fora");
+        }
+        else if(temp >=20 && temp<=30 ){
+            System.out.println("esta começado a fica quente la fora");
+        }
+        else {
+            System.out.println("esta frio la fora");
+        }
+    }
+}
