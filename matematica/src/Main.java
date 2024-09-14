29ad5f90ddc4cfe796c1d95ec7dddd173e263c49
LOURIVAL RODRIGUES DE SOUSA NETO <vexlourival1234@gmail.com>
2024-09-14 12:20:40 -0300

    Add files via upload

---
+++ matematica/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        double x = 3.90;
+        double y = 10.0;
+
+        double z = Math.max(x, y);
+        double w = Math.min(x, y);
+        double h = Math.abs(y);
+        double a = Math.sqrt(y);
+        double b = Math.round(x);
+        double c = Math.ceil(x);
+        double d = Math.floor(x);
+
+        System.out.println(h);
+        System.out.println(z);
+        System.out.println(w);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+
+
+    }
+
+
+}
