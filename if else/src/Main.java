05b5c3ef644a93f118f1610ada15e8ccb406c5f3
LOURIVAL RODRIGUES DE SOUSA NETO <vexlourival1234@gmail.com>
2024-09-14 12:29:45 -0300

    Add files via upload

---
+++ if else/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int age = 60;
+
+        if (age == 60) {
+            System.out.println("você è velho");
+
+
+        }
+        else if (age >= 18) {
+            System.out.println("você é adulto");
+        }
+        else if (age >= 13) {
+            System.out.println("você é um adolencente");
+        }
+        else {
+            System.out.println("voce não é a adulto");
+        }
+    }
+}
